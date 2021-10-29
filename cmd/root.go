3beff883766322ecea76eec48177d0c3f586d786
Stevie <4719798+transient1@users.noreply.github.com>
2021-10-29 14:00:52 -0400

    Update cmd/root.go
    Co-authored-by: Andrew Suderman <andrew@sudermanjr.com>

--- cmd/root.go
+++ cmd/root.go
@@ -68,10 +68,6 @@ func init() {
 	// will be global for your application.
 
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hall-monitor.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
