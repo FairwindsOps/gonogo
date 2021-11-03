d5d16dce19efec7d462fe6f4e2959b0bee4af972
Stevie Caldwell <4719798+transient1@users.noreply.github.com>
2021-11-03 13:55:06 -0400

    Clean up unused init and initconfig

--- cmd/root.go
+++ cmd/root.go
@@ -21,13 +21,10 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/spf13/viper"
-
 	"k8s.io/klog"
 )
 
 var (
-	cfgFile       string
 	version       string
 	versionCommit string
 )
@@ -50,45 +47,10 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute(VERSION string, COMMIT string) {
-	cobra.CheckErr(rootCmd.Execute())
 	version = VERSION
 	versionCommit = COMMIT
 	if err := rootCmd.Execute(); err != nil {
 		klog.Error(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hall-monitor.yaml)")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".hall-monitor" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".hall-monitor")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
