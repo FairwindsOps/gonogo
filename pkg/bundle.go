b12533ba2a6de1773fb5cd170d2e947e030a4f2e
Stevie Caldwell <4719798+transient1@users.noreply.github.com>
2021-11-08 10:01:33 -0500

    Fixed naming convention for structs; go fmt

--- pkg/bundle.go
+++ pkg/bundle.go
@@ -16,29 +16,24 @@ limitations under the License.
 
 package bundle
 
-import "fmt"
-
 type Source struct {
-	Chart string
+	Chart      string
 	Repository string
 }
 
 type Version struct {
 	Start string
-	End string
+	End   string
 }
 
 type Bundle struct {
-	Name string
-	Version Version
-	Notes string
-	Source Source
-	Warnings []string
-	Compatible_k8s_versions []string
-	Necessary_api_versions []string
-	Values_schame string
-	Opa_checks []string
+	Name                  string
+	Version               Version
+	Notes                 string
+	Source                Source
+	Warnings              []string
+	CompatibleK8sVersions []string
+	NecessaryApiVersions  []string
+	ValuesSchema          string
+	OpaChecks             []string
 }
-
-
-
