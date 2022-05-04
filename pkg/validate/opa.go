a7fe3bd803a64977beb6597616979382abc59c53
Stevie Caldwell <4719798+transient1@users.noreply.github.com>
2022-05-03 22:06:33 -0400

    add ability to grab objects from cluster & add to manifest list for rego processing

--- pkg/validate/opa.go
+++ pkg/validate/opa.go
@@ -4,12 +4,28 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"strings"
 
 	"github.com/fairwindsops/hall-monitor/pkg/helm"
 	"github.com/fairwindsops/insights-plugins/opa/pkg/rego"
 	"gopkg.in/yaml.v3"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
 )
+
+var clientset = helm.GetConfigInstance()
+var dynamicClient = helm.GetDynamicInstance().Client
+
+var (
+	group    string
+	version  string
+	resource string
+)
+
 
 func (m *match) RunOPAChecks() error {
 	if len(m.Bundle.OpaChecks) < 1 {
@@ -22,11 +38,38 @@ func (m *match) RunOPAChecks() error {
 		return nil
 	}
 
-	client := helm.NewHelm("")
+	resources := m.Bundle.Resources
+
+	namespaces := helm.GetNamespaces()
+
+	for _, namespace := range namespaces.Items {
+		ns := namespace.Name
+		for _, r := range resources {
+			rs := strings.SplitAfter(r, "/")
+
+			if len(rs) == 3 {
+				group = rs[0]
+				version = rs[1]
+				resource = rs[2]
+			} else {
+				group = ""
+				version = rs[0]
+				resource = rs[1]
+			}
+
+			objs, err := GetClusterObjects(dynamicClient, context.TODO(), group, version, resource, ns)
+			if err != nil {
+				klog.Error()
+			}
+			for _, i := range objs {
+				manifests = append(manifests, i.Object)
+			}
+		}
+	}
 
 	for _, o := range m.Bundle.OpaChecks {
 		for _, y := range manifests {
-			r, err := rego.RunRegoForItem(context.TODO(), o, nil, y, client, nil)
+			r, err := rego.RunRegoForItem(context.TODO(), o, nil, y, clientset, nil)
 			if err != nil {
 				klog.Error(err)
 				continue
@@ -70,4 +113,18 @@ func splitYAML(objects []byte) ([]map[string]interface{}, error) {
 		output = append(output, value)
 	}
 	return output, nil
+}
+
+func GetClusterObjects(dynamic dynamic.Interface, ctx context.Context, group string, version string, resource string, namespace string) ([]unstructured.Unstructured, error) {
+	resourceId := schema.GroupVersionResource{
+		Group:    group,
+		Version:  version,
+		Resource: resource,
+	}
+	list, err := dynamic.Resource(resourceId).Namespace(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		klog.Error(err)
+	}
+
+	return list.Items, nil
 }
