46da4c7e954c7b84a8c234927c4141d7b7163f26
Stevie Caldwell <4719798+transient1@users.noreply.github.com>
2022-06-29 14:18:38 -0400

    Added check for length of min and max values in bundle

--- pkg/validate/versions.go
+++ pkg/validate/versions.go
@@ -7,30 +7,42 @@ import (
 )
 
 func (m *match) validateClusterVersion(cv *clusterVersion.Info) error {
+	var maxVer, minVer, clusterVer semver.Version
+	var err error
 
-	maxVer, err := semver.ParseTolerant(m.Bundle.CompatibleK8sVersions.Max)
+	clusterVer, err = semver.ParseTolerant(cv.String())
 	if err != nil {
 		klog.Error(err)
 	}
 
-	minVer, err := semver.ParseTolerant(m.Bundle.CompatibleK8sVersions.Min)
-	if err != nil {
-		klog.Error(err)
+	if len(m.Bundle.CompatibleK8sVersions.Max) != 0 {
+		maxVer, err = semver.ParseTolerant(m.Bundle.CompatibleK8sVersions.Max)
+		if err != nil {
+			klog.Error(err)
+		}
+		if clusterVer.GT(maxVer) {
+			m.AddonOutput.ActionItems = append(m.AddonOutput.ActionItems, &ActionItem{
+				ResourceNamespace: m.Release.Namespace,
+				ResourceName:      m.Release.Name,
+				Title:             "Unsupported cluster version",
+				Description:       "The Kubernetes cluster version is greater than the maximum version specified in the bundle spec",
+			})
+		}
 	}
 
-	clusterVer, err := semver.ParseTolerant(cv.String())
-	if err != nil {
-		klog.Error(err)
-	}
-
-	if clusterVer.LT(minVer) || clusterVer.GT(maxVer) {
-		m.AddonOutput.ActionItems = append(m.AddonOutput.ActionItems, &ActionItem{
-			ResourceNamespace: m.Release.Namespace,
-			ResourceName:      m.Release.Name,
-			Title:             "Unsupported cluster version",
-			Description:       "The Kubernetes cluster version is less or greater than the minimum and maximum versions specified in the bundle spec",
-		})
-
+	if len(m.Bundle.CompatibleK8sVersions.Min) != 0 {
+		minVer, err = semver.ParseTolerant(m.Bundle.CompatibleK8sVersions.Min)
+		if err != nil {
+			klog.Error(err)
+		}
+		if clusterVer.LT(minVer) {
+			m.AddonOutput.ActionItems = append(m.AddonOutput.ActionItems, &ActionItem{
+				ResourceNamespace: m.Release.Namespace,
+				ResourceName:      m.Release.Name,
+				Title:             "Unsupported cluster version",
+				Description:       "The Kubernetes cluster version is less than the minimum version specified in the bundle spec",
+			})
+		}
 	}
 	return nil
 }
